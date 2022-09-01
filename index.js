8b0411421aaf851dfaa02838e0a3be6379dbe49a
suryaprakashr1025 <110890454+suryaprakashr1025@users.noreply.github.com>
2022-09-01 14:43:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function foo(){
+var first = document.getElementById("fn").value;
+console.log(first);
+var middle = document.getElementById("mn").value;
+console.log(middle);
+var last = document.getElementById("ln").value;
+console.log(last);
+var ema = document.getElementById("em").value;
+console.log(ema);
+}
